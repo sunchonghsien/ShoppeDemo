9293c947c622b722b74bb43c0f770f0b7d79d145
sunchonghsien <abc15cd5@gmail.com>
2022-07-26 15:33:50 +0800

    create project

---
+++ src/main/java/com/example/Controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class IndexController {
+
+    @GetMapping(value = "")
+    public String home(){
+        return "123";
+    }
+
+    @GetMapping(value = "login")
+    public String login(){
+        return "hello";
+    }
+}
